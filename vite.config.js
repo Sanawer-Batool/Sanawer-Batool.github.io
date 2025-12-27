af2e9f6eebc26a64352f64ec20dd4e8df513016c
Sanawer-Batool <sanawerb246@gmail.com>
2025-12-27 12:17:17 +0500

    fixed github page deployment

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/", // ✅ REQUIRED for Sanawer-Batool.github.io
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
